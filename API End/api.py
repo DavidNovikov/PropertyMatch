ac8f6fb31a44d9d4f494386a92375b2c84583243
HarinieJ <74274095+HarinieJ@users.noreply.github.com>
2020-11-14 19:51:54 -0500

    jsonfiles (#4)
    Co-authored-by: unknown <jeyabalan.1@osu.edu>

--- API End/api.py
+++ API End/api.py
@@ -8,49 +8,70 @@ app.config["DEBUG"] = True
 properties = [
     {'id': 0,
      'area': 'Cleveland',
-     'flood likelihood': '75%',
-     'crime data': '50% above Ohio',
-     'disasters': '25% likely'},
+     'damage to property due to storm events in 2019: '25.719M cases',
+     'annual violent crime data reported per capita': '6203 cases',
+     'annual violent crime data cleared per capita': '961 cases',
+     'annual property crime data reported per capita': '17057 cases',
+     'annual property crime data cleared per capita': '526 cases',
+     },
     {'id': 1,
      'area': 'Columbus',
-     'flood likelihood': '50%',
-     'crime data': '75% above Ohio',
-     'disasters': '25% likely'},
+     'damage to property due to storm events in 2019': '178.8K cases',
+     'annual violent crime data reported per capita': '5350 cases',
+     'annual violent crime data cleared per capita': '795 cases',
+     'annual property crime data reported per capita': '29974 cases',
+     'annual property crime data cleared per capita': '1240 cases,
+     },
     {'id': 2,
      'area': 'Cincinnati',
-     'flood likelihood': '10%',
-     'crime data': '60% above Ohio',
-     'disasters': '30% likely'},
+     'damage to property due to storm events in 2019': '346.2K cases',
+     'annual violent crime data reported per capita': '2824 cases',
+     'annual violent crime data cleared per capita': '1229 cases',
+     'annual property crime data reported per capita': '13051 cases',
+     'annual property crime data cleared per capita': '2362 cases',
+     },
     {'id': 3,
      'area': 'Toledo',
-     'flood likelihood': '60%',
-     'crime data': '75% above Ohio',
-     'disasters': '40% likely'},
+     'damage to property due to storm events in 2019': '15.37M cases',
+     'annual violent crime data reported per capita': '2784 cases',
+     'annual violent crime data cleared per capita': '846 cases',
+     'annual property crime data reported per capita': '9470 cases',
+     'annual property crime data cleared per capita': '885 cases',
+     },
     {'id': 4,
      'area': 'Dayton',
-     'flood likelihood': '10%',
-     'crime data': '30% above Ohio',
-     'disasters': '20% likely'},
+     'damage to property due to storm events in 2019': '61.8K cases',
+     'annual violent crime data reported per capita': '1519 cases',
+     'annual violent crime data cleared per capita': '558 cases',
+     'annual property crime data reported per capita': '5673 cases',
+     'annual property crime data cleared per capita': '584 cases',
+     },
     {'id': 5,
      'area': 'Ohio',
-     'flood likelihood': '50%',
-     'crime data': '50% above the nation',
-     'disasters': '10% likely'},
+     'damage to property due to storm events in 2019': '54.682M cases',
+     'annual violent crime data per capita': '293.2',
+     'annual property crime data per capita': '2055.7',
+     },
+    {'id': 6,
+     'area': 'United States',
+     'violent crime data': '379.4',
+     'property crime data': '2109.9',
+     },
 ]
 
 
 @app.route('/', methods=['GET'])
 def home():
-    return '''<h1>Distant Reading Archive</h1>
-<p>A prototype API for distant reading of science fiction novels.</p>'''
+    return '''<h1>Property Area Statistics</h1>
+<p>This API has been created to show the stats of cities in Ohio compared to the United States.</p>'''
 
 
-@app.route('/api/v1/resources/books/all', methods=['GET'])
+@app.route('/api/v1/resources/properties/all', methods=['GET'])
 def api_all():
     return jsonify(properties)
 
 
-@app.route('/api/v1/resources/books', methods=['GET'])
+@app.route('/api/v1/resources/properties', methods=['GET'])
 def api_id():
     # Check if an ID was provided as part of the URL.
     # If ID is provided, assign it to a variable.
