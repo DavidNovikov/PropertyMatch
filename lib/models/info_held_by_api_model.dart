ade26133e723522583b91196e79192b921d93909
unknown <jacobmatt1127@gmail.com>
2020-11-14 21:47:09 -0500

    attempt at IO

--- lib/models/info_held_by_api_model.dart
+++ lib/models/info_held_by_api_model.dart
@@ -1,22 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:property_match/models/tab_info.dart';
+import 'dart:io';
+import 'dart:convert';
 
 class InfoHeldByApiModel extends ChangeNotifier {
   InfoHeldByApiModel() {
-    infoMap = {
-      'Cleveland': TabInfo(
-        'Cleveland',
-        {'Crime rate': "4.0"},
-      ),
-      'Columbus': TabInfo(
-        'Columbus',
-        {'Crime rate': "3.0"},
-      ),
-      'Toledo': TabInfo(
-        'Toledo',
-        {'Crime rate': "5.0"},
-      ),
-    };
+    File read = new File('./PropertyMatch/API End/data.txt');
+    Future<String> futureContent = read.readAsString();
+    Map valueMap = json.decode(value);
+    print(valueMap.toString);
+    // infoMap = {
+    //   'Cleveland': TabInfo(
+    //     'Cleveland',
+    //     {'Crime rate': "4.0"},
+    //   ),
+    //   'Columbus': TabInfo(
+    //     'Columbus',
+    //     {'Crime rate': "3.0"},
+    //   ),
+    //   'Toledo': TabInfo(
+    //     'Toledo',
+    //     {'Crime rate': "5.0"},
+    //   ),
+    // };
   }
 
   Map<String, TabInfo> infoMap = Map<String, TabInfo>();
